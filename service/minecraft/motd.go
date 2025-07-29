def783ee3a79be3923c68b746f6a2dba6e66343e
SaegusaMayumi1234 <69360824+SaegusaMayumi1234@users.noreply.github.com>
2025-07-29 18:01:36 +0700

    Change protocol name

--- service/minecraft/motd.go
+++ service/minecraft/motd.go
@@ -35,7 +35,7 @@ func generateMOTD(protocolVersion int, s *config.ConfigProxyService, options *tr
 			Name     string `json:"name"`
 			Protocol int    `json:"protocol"`
 		}{
-			Name:     "ZBProxy " + version.Version,
+			Name:     "Unknown " + version.Version,
 			Protocol: protocolVersion,
 		},
 		Players: struct {
